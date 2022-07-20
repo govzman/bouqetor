3556db5ed560d5a70fee24303d3110e40cdbe3de
akulen0k <andreitrokhachev@yandex.ru>
2022-07-20 15:23:29 +0300

    Version 2.2.4

--- app/src/main/java/com/example/hse_app/Bouquets.kt
+++ app/src/main/java/com/example/hse_app/Bouquets.kt
@@ -1,9 +1,9 @@
 package com.example.hse_app
 
-enum class Flowers {
-    Rose, Chamomile
-}
-
-class Bouquets {
-
+class Bouquets (var radius : Int = 100, var all_cnt : Int = 0, var text : String = ""){
+    var current_flowers : MutableList<Flower> = ArrayList()
+    var cnt_flowers : MutableList<Int> = ArrayList()
+    init {
+        for (i in 0 until Flowers.values().size) cnt_flowers += 0
+    }
 }
---
+++ app/src/main/java/com/example/hse_app/Flower.kt
@@ -0,0 +1,7 @@
+package com.example.hse_app
+
+enum class Flowers{
+    Rose, Сhamomile
+}
+
+class Flower (val name : Flowers, var x : Int, var y : Int)
