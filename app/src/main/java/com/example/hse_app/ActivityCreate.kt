140c127d772103fbbe1c1a0492f51bb58c47ad84
akulen0k <andreitrokhachev@yandex.ru>
2022-07-22 14:48:19 +0300

    Version 3.1.1

--- app/src/main/java/com/example/hse_app/ActivityCreate.kt
+++ app/src/main/java/com/example/hse_app/ActivityCreate.kt
@@ -3,10 +3,7 @@ package com.example.hse_app
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.MotionEvent
-import android.view.View
+import android.view.*
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
