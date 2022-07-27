36cad13b6da97f1ff17b3a3ed7d80ddcabcaa097
akulen0k <andreitrokhachev@yandex.ru>
2022-07-27 13:18:24 +0300

    Version 3.8

--- app/src/main/java/com/example/hse_app/ActivityInfo.kt
+++ app/src/main/java/com/example/hse_app/ActivityInfo.kt
@@ -17,6 +17,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.gson.GsonBuilder
 
 class ActivityInfo : AppCompatActivity() {
+    var cur_b : Bouquets = Bouquets("null")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activityinfo)
@@ -27,6 +28,34 @@ class ActivityInfo : AppCompatActivity() {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowHomeEnabled(true)
         }
-        // не отображает старые значения
+        var name : String = intent.getStringExtra("name") ?: ""
+        var raw : String? = intent.getStringExtra("bouq") ?: null
+        if (raw != null) {
+            val gson = GsonBuilder().create()
+            cur_b = gson.fromJson(raw, Bouquets::class.java)
+        }
+        var txtv : TextView = findViewById(R.id.textView)
+        var cur_t : String = ""
+        var cnt = 0
+        var price = 0
+        for (i in Flowers_print.values()) {
+            try {
+                var cur_f: String = ""
+                cur_f += i.toString()
+                cur_f += ": "
+                cur_f += cur_b.cnt_flowers[cnt].toString()
+                price += cur_b.cnt_flowers[cnt] * costs[cnt]
+                cur_f += "\n"
+                if (cur_b.cnt_flowers[cnt] != 0) cur_t += cur_f
+                ++cnt
+            }
+            catch(e : Exception) {
+                break
+            }
+        }
+        cur_t += "Total price: "
+        cur_t += price.toString()
+        cur_t += " rub"
+        txtv.setText(cur_t)
     }
 }
--- app/src/main/java/com/example/hse_app/Flower.kt
+++ app/src/main/java/com/example/hse_app/Flower.kt
@@ -4,5 +4,10 @@ enum class Flowers{
     rose, chamomile, carnation, chrysanthemum, iris, peony, lily, sunflower, hortensia, gypsophila, ruscus, dianthus, trachelium
 }
 
+enum class Flowers_print {
+    Roses, Chamomiles, Carnations, Chrysanthemums, Irises, Peonies, Lilies, Sunflowers, Hortensias, Gypsophilas, Ruscuses, Dianthuses, Tracheliums
+}
 val bans : List<String> = listOf("Settings")
+var costs : List<Int> = listOf(150, 120, 60, 150, 80, 300, 525, 120, 525, 150, 60, 120, 120)
+
 class Flower(val name: Flowers = Flowers.rose, var x: Float = 0.toFloat(), var y: Float = 0.toFloat(), var size: Int = 0)
