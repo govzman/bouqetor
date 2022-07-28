4c347bfd8f8860ca683805ff18ba9798aff65500
Ilya <govzman@list.ru>
2022-07-29 00:48:19 +0300

    add autosave to Editor

--- app/src/main/java/com/example/hse_app/ActivityEdtior.kt
+++ app/src/main/java/com/example/hse_app/ActivityEdtior.kt
@@ -167,10 +167,6 @@ class ActivityEditor : AppCompatActivity(), View.OnTouchListener, View.OnClickLi
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_save) {
-//            for (i in 0 until current_bouquet.current_flowers.size) {
-//                current_bouquet.current_flowers[i].x = findViewById<ImageView>(i + 1).x
-//                current_bouquet.current_flowers[i].y = findViewById<ImageView>(i + 1).y
-//            }
             current_bouquet = Bouquets(current_bouquet.name)
             for (i in 0 until all_id.size) {
                 var current_flower: Flower = Flower(
@@ -180,7 +176,6 @@ class ActivityEditor : AppCompatActivity(), View.OnTouchListener, View.OnClickLi
                     findViewById<ImageView>(all_id[i]).height
                 )
 
-//                Log.i("SAVE1", current_)
                 if (all_types[i] == Flowers.rose) current_bouquet.cnt_flowers[0] += 1
                 else if (all_types[i] == Flowers.chamomile) current_bouquet.cnt_flowers[1] += 1
                 else if (all_types[i] == Flowers.carnation) current_bouquet.cnt_flowers[2] += 1
@@ -226,6 +221,39 @@ class ActivityEditor : AppCompatActivity(), View.OnTouchListener, View.OnClickLi
                 all_id.removeLast()
                 all_types.removeLast()
             }
+        }
+        else if (item.itemId == android.R.id.home){
+            Log.i("BACK", "HOME")
+
+            current_bouquet = Bouquets(current_bouquet.name)
+            for (i in 0 until all_id.size) {
+                var current_flower: Flower = Flower(
+                    all_types[i],
+                    findViewById<ImageView>(all_id[i]).x,
+                    findViewById<ImageView>(all_id[i]).y,
+                    findViewById<ImageView>(all_id[i]).height
+                )
+
+                if (all_types[i] == Flowers.rose) current_bouquet.cnt_flowers[0] += 1
+                else if (all_types[i] == Flowers.chamomile) current_bouquet.cnt_flowers[1] += 1
+                else if (all_types[i] == Flowers.carnation) current_bouquet.cnt_flowers[2] += 1
+                else if (all_types[i] == Flowers.chrysanthemum) current_bouquet.cnt_flowers[3] += 1
+                else if (all_types[i] == Flowers.peony) current_bouquet.cnt_flowers[4] += 1
+                else if (all_types[i] == Flowers.iris) current_bouquet.cnt_flowers[5] += 1
+                else if (all_types[i] == Flowers.lily) current_bouquet.cnt_flowers[6] += 1
+                else if (all_types[i] == Flowers.hortensia) current_bouquet.cnt_flowers[7] += 1
+                else if (all_types[i] == Flowers.sunflower) current_bouquet.cnt_flowers[8] += 1
+                else if (all_types[i] == Flowers.ruscus) current_bouquet.cnt_flowers[9] += 1
+                else if (all_types[i] == Flowers.dianthus) current_bouquet.cnt_flowers[10] += 1
+                else if (all_types[i] == Flowers.trachelium) current_bouquet.cnt_flowers[11] += 1
+                current_bouquet.current_flowers += current_flower
+                current_bouquet.all_cnt += 1
+            }
+            MySave(current_bouquet)
+            val new_activity = Intent(this@ActivityEditor, ActivityView::class.java)
+            new_activity.putExtra("name", current_bouquet.name)
+            startActivity(new_activity)
+            return true
         }
         else if (item.itemId != R.id.filter_flowers) {
             var newView: ImageView
